fa202ea6142c5525dfbe28d9f8fbf40c8a245dbf
mm10016 <mm10016@st.habib.edu.pk>
2025-12-06 21:37:12 +0500

    Update FastEnemy.cpp
    by Mohsin

--- FastEnemy.cpp
+++ FastEnemy.cpp
@@ -1,12 +1,24 @@
 #include "FastEnemy.hpp"
 
+// Constructor: Creates a fast enemy at specified position
+// pos: Starting position of the enemy
+// Calls base Enemy constructor with:
+// - pos: position
+// - 160.f: higher speed (160 pixels/second, vs basic enemy's 90)
+// - 2: higher health (2 hits to kill, vs basic enemy's 1)
+// - "assets/fast_enemy.png": different texture file for visual distinction
 FastEnemy::FastEnemy(const sf::Vector2f& pos) 
-    : Enemy(pos, 160.f, 2, "assets/fast_enemy.png") {}
+    : Enemy(pos, 160.f, 2, "assets/fast_enemy.png") {
+}
 
+// Update function for fast enemy
+// dt: Delta time for frame-independent updates
 void FastEnemy::update(float dt) {
+    // Simply update sprite position to match entity position
     sprite.setPosition(position);
 }
 
+// Returns the specific entity type for this enemy
 Entity::Type FastEnemy::getType() const { 
-    return Entity::Type::FAST_ENEMY; 
+    return Entity::Type::FAST_ENEMY; // Distinguishes from BASIC_ENEMY type
 }
